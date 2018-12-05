97f4701c17e21b370337ed57429ec3c542653fbd
majed ben ali <mr.majedbenali@gmail.com>
2018-12-05 15:02:56 +0100

    first commit , créeation projet spring

---
+++ src/main/java/org/sid/mehatIntervention/MehatInterventionApplication.java
@@ -0,0 +1,12 @@
+package org.sid.mehatIntervention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehatInterventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehatInterventionApplication.class, args);
+	}
+}
